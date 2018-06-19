d27a2ede24524f93f657f151862c17beb565c95a
soshi.matsuo <soshi.matsuo@rakuten.com>
2018-06-19 04:03:48 +0000

    want ranking

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::delete('have', 'ItemUserController@dont_have')->name('item_user.dont_have');
     Route::resource('users', 'UsersController', ['only' => ['show']]);
 });
+
+// Ranking
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+
